5fdcafc6dc37512b7103d87ce6d42439c6f08c8f
jeffprestes_janus <jefferson@janusproj.com>
2019-06-06 13:47:24 -0300

    Generalizando o projeto

---
+++ connectToSmartContract.js
@@ -0,0 +1,42 @@
+
+var web3;
+var contract;
+var network;
+var contractAddress;
+
+if (window.ethereum) {
+    web3 = new Web3(ethereum);
+    try {
+        // Solicita acesso a carteira Ethereum se necessário
+        ethereum.enable();    
+        network = web3.version.network;
+    } catch (err) { // Usuário ainda não deu permissão para acessar a carteira Ethereum    
+        console.log("erro"); 
+        console.error(err);           
+    }
+}
+
+function waitForTxToBeMined(txHash, objStatus) {
+    let txReceipt;
+    web3.eth.getTransactionReceipt(txHash, function (err, result) {
+        if (err) {
+            console.error(err);
+            return;
+        }        
+        txReceipt = result;
+        let d = new Date();
+        console.log("txHash: " + txHash + " at " + d);
+        console.log(txReceipt); 
+        if (txReceipt) {
+            if (txReceipt.status == "0x1") {
+                $(objStatus).css("background-color", "LawnGreen");
+                $(objStatus).html("Record successfuly saved at block: " + txReceipt.blockNumber + " - Transaction hash: " + txHash);            
+            } else {
+                $(objStatus).css("background-color", "Salmon");
+                $(objStatus).html("There was an error - Transaction hash: " + txHash + " final status: " + txReceipt.status);            
+            }
+        } else {
+            window.setTimeout(waitForTxToBeMined, 1500, txHash, objStatus);
+        }
+    });
+}
